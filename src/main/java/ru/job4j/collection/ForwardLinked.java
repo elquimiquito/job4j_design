e9d50b537f261f1d7194215576fd3d5adea07744
elquimiquito <whiteheaded12@gmail.com>
2023-02-16 20:39:16 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,88 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> oldNode = head;
+            while (oldNode.next != null) {
+                oldNode = oldNode.next;
+            }
+            oldNode.next = newNode;
+        }
+        modCount++;
+        size++;
+    }
+
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> rsl = head;
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return rsl.item;
+    }
+
+    public T deleteFirst() {
+        T rsl;
+        if (head != null) {
+            Node<T> newHead = head.next;
+            rsl = head.item;
+            head.next = null;
+            head = newHead;
+        } else {
+            throw new NoSuchElementException();
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            Node<T> pointer = head;
+            int expectedMod = modCount;
+            @Override
+            public boolean hasNext() {
+                if (expectedMod != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return pointer != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                var rsl = pointer.item;
+                pointer = pointer.next;
+                return rsl;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
