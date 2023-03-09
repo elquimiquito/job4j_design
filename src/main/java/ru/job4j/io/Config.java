c2bdfd8eda1e646ddfeb131c3d8b3b0ca5fd1b8a
elquimiquito <whiteheaded12@gmail.com>
2023-03-09 18:36:04 +0300

    6. Читаем файл конфигурации [#858 #397114]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -23,7 +23,7 @@ public class Config {
                     .map(line -> line.split("=", 2))
                     .forEach(
                             array -> {
-                                if (array.length != 2 & array[0].isBlank() || array[1].isBlank()) {
+                                if (array.length != 2 || array[0].isBlank() || array[1].isBlank()) {
                                     throw new IllegalArgumentException();
                                 }
                                 values.put(array[0], array[1]);
